652a46eada200d2acc47286c2becfa72056c2a64
HzHaxx <hamberzq@gmail.com>
2024-09-25 11:04:06 -0500

    Mostrar imágenes y Modal

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,50 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   crearGaleria();
 });
 
 function crearGaleria() {
-    const CANTIDAD_IMAGENES = 16;
-    const galeria = document.querySelector('.galeria-imagenes');
+  const CANTIDAD_IMAGENES = 16;
+  const galeria = document.querySelector('.galeria-imagenes');
 
-    for(let i = 1; i <= CANTIDAD_IMAGENES; i++) {
-        const imagen = document.createElement('IMG');
-        imagen.src = `src/img/gallery/full/${i}.jpg`;
-        imagen.alt = 'Imagen Galeria';
+  for (let i = 1; i <= CANTIDAD_IMAGENES; i++) {
+    const imagen = document.createElement('IMG');
+    imagen.src = `src/img/gallery/full/${i}.jpg`;
+    imagen.alt = 'Imagen Galeria';
 
-        galeria.appendChild(imagen);
+    // Event Handler
+    imagen.onclick = function () {
+      mostrarImagen(i);
     }
+
+    galeria.appendChild(imagen);
+  }
+}
+
+function mostrarImagen(i) {
+  const imagen = document.createElement('IMG');
+  imagen.src = `src/img/gallery/full/${i}.jpg`;
+  imagen.alt = 'Imagen Galeria';
+
+  // Generar Modal
+  const modal = document.createElement('DIV');
+  modal.classList.add('modal');
+  modal.onclick = cerrarModal;
+  modal.appendChild(imagen);
+
+  // Agregar al HTML
+  const body = document.querySelector('body');
+  body.classList.add('overflow-hidden');
+  body.appendChild(modal);
+}
+
+function cerrarModal() {
+  const modal = document.querySelector('.modal');
+  modal.classList.add('fade-out');
+  
+  setTimeout(() => {
+    modal.remove();
+
+    const body = document.querySelector('body');
+    body.classList.remove('overflow-hidden');
+  }, 500);
 }
