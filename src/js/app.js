0ed7f7243e3db613f06037d4ab0a80fdc35cbde1
HzHaxx <hamberzq@gmail.com>
2024-10-06 16:46:22 -0500

    Corrections

--- src/js/app.js
+++ src/js/app.js
@@ -1,119 +1,120 @@
 document.addEventListener('DOMContentLoaded', function () {
-  navegacionFija();
-  crearGaleria();
-  resaltarEnlace();
-  scroollNav();
-});
+  navegacionFija()
+  crearGaleria()
+  resaltarEnlace()
+  scrollNav()
+})
 
 function navegacionFija() {
-  const header = document.querySelector('.header');
-  const sobreFestival = document.querySelector('.sobre-festival');
+  const header = document.querySelector('.header')
+  const sobreFestival = document.querySelector('.sobre-festival')
 
   document.addEventListener('scroll', function () {
     if (sobreFestival.getBoundingClientRect().bottom < 1) {
-      header.classList.add('fixed');
+      header.classList.add('fixed')
     } else {
-      header.classList.remove('fixed');
+      header.classList.remove('fixed')
     }
-  });
+  })
 }
 
 function crearGaleria() {
-  const CANTIDAD_IMAGENES = 16;
-  const galeria = document.querySelector('.galeria-imagenes');
+
+  const CANTIDAD_IMAGENES = 16
+  const galeria = document.querySelector('.galeria-imagenes')
 
   for (let i = 1; i <= CANTIDAD_IMAGENES; i++) {
-    const imagen = document.createElement('PICTURE');
+    const imagen = document.createElement('PICTURE')
     imagen.innerHTML = `
-      <source srcset="build/img/gallery/thumb/${i}.avif" type="image/avif">
-      <source srcset="build/img/gallery/thumb/${i}.webp" type="image/webp">
-      <img loading="lazy" width="200" height="300" src="build/img/gallery/thumb/${i}.jpg" alt="imagen galeria">
-    `;
+          <source srcset="build/img/gallery/thumb/${i}.avif" type="image/avif">
+          <source srcset="build/img/gallery/thumb/${i}.webp" type="image/webp">
+          <img loading="lazy" width="200" height="300" src="build/img/gallery/thumb/${i}.jpg" alt="imagen galeria">
+      `;
 
     // Event Handler
     imagen.onclick = function () {
-      mostrarImagen(i);
+      mostrarImagen(i)
     }
 
-    galeria.appendChild(imagen);
+    galeria.appendChild(imagen)
   }
 }
 
 function mostrarImagen(i) {
-  const imagen = document.createElement('PICTURE');
+  const imagen = document.createElement('PICTURE')
   imagen.innerHTML = `
       <source srcset="build/img/gallery/full/${i}.avif" type="image/avif">
       <source srcset="build/img/gallery/full/${i}.webp" type="image/webp">
       <img loading="lazy" width="200" height="300" src="build/img/gallery/full/${i}.jpg" alt="imagen galeria">
-    `;
+  `;
 
   // Generar Modal
-  const modal = document.createElement('DIV');
-  modal.classList.add('modal');
-  modal.onclick = cerrarModal;
+  const modal = document.createElement('DIV')
+  modal.classList.add('modal')
+  modal.onclick = cerrarModal
 
   // Botón cerrar modal
-  const cerrarModalBtn = document.createElement('BUTTON');
-  cerrarModalBtn.textContent = 'X';
-  cerrarModalBtn.classList.add('btn-cerrar');
-  modal.onclick = cerrarModal;
+  const cerrarModalBtn = document.createElement('BUTTON')
+  cerrarModalBtn.textContent = 'X'
+  cerrarModalBtn.classList.add('btn-cerrar')
+  cerrarModalBtn.onclick = cerrarModal
 
-  modal.appendChild(imagen);
-  modal.appendChild(cerrarModalBtn);
+  modal.appendChild(imagen)
+  modal.appendChild(cerrarModalBtn)
 
   // Agregar al HTML
-  const body = document.querySelector('body');
-  body.classList.add('overflow-hidden');
-  body.appendChild(modal);
+  const body = document.querySelector('body')
+  body.classList.add('overflow-hidden')
+  body.appendChild(modal)
+
 }
 
 function cerrarModal() {
-  const modal = document.querySelector('.modal');
-  modal.classList.add('fade-out');
+
+  const modal = document.querySelector('.modal')
+  modal.classList.add('fade-out')
 
   setTimeout(() => {
-    modal?.remove();
+    modal?.remove()
 
-    const body = document.querySelector('body');
-    body.classList.remove('overflow-hidden');
+    const body = document.querySelector('body')
+    body.classList.remove('overflow-hidden')
   }, 500);
 }
 
 function resaltarEnlace() {
   document.addEventListener('scroll', function () {
-    const sections = document.querySelectorAll('section');
-    const navLinks = document.querySelectorAll('.navegacion-principal a');
+    const sections = document.querySelectorAll('section')
+    const navLinks = document.querySelectorAll('.navegacion-principal a')
 
     let actual = '';
     sections.forEach(section => {
-      const sectionTop = section.offsetTop;
-      const sectionHeight = section.clientHeight;
-
+      const sectionTop = section.offsetTop
+      const sectionHeight = section.clientHeight
       if (window.scrollY >= (sectionTop - sectionHeight / 3)) {
-        actual = section.id;
+        actual = section.id
       }
-    });
+    })
 
     navLinks.forEach(link => {
-      link.classList.remove('active');
-
+      link.classList.remove('active')
       if (link.getAttribute('href') === '#' + actual) {
-        link.classList.add('active');
+        link.classList.add('active')
       }
-    });
-  });
+    })
+  })
 }
 
 function scrollNav() {
-  const navLinks = document.querySelectorAll('.navegacion-principal a');
+  const navLinks = document.querySelectorAll('.navegacion-principal a')
 
   navLinks.forEach(link => {
-    link.addEventListener('click', function (e) {
-      e.preventDefault();
-      const sectionScroll = e.target.getAttribute('href');
-      const section = document.querySelector(sectionScroll);
+    link.addEventListener('click', e => {
+      e.preventDefault()
+      const sectionScroll = e.target.getAttribute('href')
+      const section = document.querySelector(sectionScroll)
 
-      section.scrollIntoView({ behavior: 'smooth' });
-    });
-  });
+      section.scrollIntoView({ behavior: 'smooth' })
+    })
+  })
 }
